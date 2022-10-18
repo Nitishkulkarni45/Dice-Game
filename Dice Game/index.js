1d333f5e95d5a2769ac26acfb52f9907b4e4febc
Nitish D Kulkarni <83655542+Nitishkulkarni45@users.noreply.github.com>
2022-10-18 16:29:53 +0530

    Add files via upload

---
+++ Dice Game/index.js
@@ -0,0 +1,24 @@
+var player1 = prompt ("Enter the name of player1:");
+var player2 = prompt ("Enter the name of player2:");
+var randomNumber1 = Math.floor(Math.random()*6)+1; // generates the random nuber between 1-6
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = player1 + " wins 🚩";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML = player2 + " wins 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
